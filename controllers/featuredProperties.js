fafb9eeb431554019f8104940b1453aaa14712d8
CarlosGiovannyG <cggualtero@hotmail.com>
2022-11-02 02:25:11 -0500

    Correcciones de código y aumento en los datos en la DB

--- controllers/featuredProperties.js
+++ controllers/featuredProperties.js
@@ -2,7 +2,7 @@ import { catchAsyncErrors } from "../middleware";
 import { Property } from "../models";
 
 const featuredProperties = catchAsyncErrors(async (req, res, next) => {
-  const data = await Property.find().limit(10).select("json_data").select("name").select("url").select("city")
+  const data = await Property.find().limit(20).select("json_data").select("name").select("url").select("city")
   res.status(200).json({
     success: true,
     data,
